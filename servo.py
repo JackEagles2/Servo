352ed4c8eb226d47dff49a70ef10c2c3a2c77246
JackEagles2 <jack.eagles@hotmail.co.uk>
2024-06-03 16:52:09 +0100

    yes

--- servo.py
+++ servo.py
@@ -1,25 +1,24 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 import requests  # Import the requests library to make HTTP requests
 
 print("Setup")
-<<<<<<< HEAD
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 p = GPIO.PWM(11, 50)
 p.start(0)
-=======
+
 #GPIO.setmode(GPIO.BOARD)
 #GPIO.setup(11, GPIO.OUT)
 #p = GPIO.PWM(11, 50)
 #p.start(0)
 print("Starting")
->>>>>>> 1f1b6d85251b8826e3123376b6cb4164253a6afd
 
 # Function to get PID controller output from endpoint
 def get_pid_output():
     # Replace "PID_ENDPOINT_URL" with the actual endpoint URL
-    endpoint_url = "http://192.168.0.91:5000/get_pid_output"
+    endpoint_url = "localhost:5000/get_pid_output"
     try:
         response = requests.get(endpoint_url, timeout=5)
         if response.status_code == 200:
@@ -38,16 +37,13 @@ try:
         print(pid_output)
         if pid_output is not None:
             # Scale the PID output to match servo PWM duty cycle range (0-100)
-<<<<<<< HEAD
             p.ChangeDutyCycle(pid_output)
-=======
             scaled_output = pid_output * 10  # Adjust scaling factor as needed
 
             # Ensure scaled output is within valid range
             scaled_output = max(0, min(100, scaled_output))
 
             #p.ChangeDutyCycle(scaled_output)
->>>>>>> 1f1b6d85251b8826e3123376b6cb4164253a6afd
             sleep(0.1)  # Adjust sleep time as needed for responsiveness
         else:
             # If PID output is not available, stop the servo
