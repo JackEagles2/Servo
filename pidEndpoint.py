0a2534bba9c1d5fe403076af9d057d0971f8780d
JackEagles2 <jack.eagles@hotmail.co.uk>
2024-06-06 19:38:14 +0100

    hi

--- pidEndpoint.py
+++ pidEndpoint.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, jsonify, request
 from pid import PID as pd
 from multiprocessing import Value
@@ -10,33 +12,32 @@ y = 0
 width = 720
 x_center = width / 2
 max = x_center / 10
-range = max * 2
+
 
 pid_output = Value('d', 0.0)
 pid_x = pd(Kp=0.2, Ki=0, Kd=0.01, target=0, min=0, max=0)
 
 
 def updatePD():
+    previous_number = 200
     while True:
-        # Model code to get x coordinate
-        # Get image
-        e_time = time.time() % 40
-        if e_time <= 20:
-            y = (360 / 20) * e_time
-        else:
-            y = 360 - (360 / 20) * (e_time - 20)
+        x = random.randint(100, 300)
+        #print("hi" + str(x))
+        for number in range(previous_number, x):
+            pid_error = 200 - number
+            #print(pid_error)
+            pid_output_scaled = pid_x.update(pid_error)
+            pid_output.value = pid_output_scaled
+            
 
-        #y = 360
+            time.sleep(0.1)  # Small delay to observe changes
+        previous_number = x
 
-        max_x, min_x, _, _ = (y, y, 0, 0)  # Replace with model output
-        x = (max_x + min_x) / 2
-
-        pid_error = x_center - x
-        pid_output_scaled = pid_x.update(pid_error) / max * 6 + max / 6
-        pid_output.value = pid_output_scaled
 
 pid_process = multiprocessing.Process(target=updatePD)
 pid_process.start()
+
+
 # Endpoint to get PID output
 @app.route('/get_pid_output', methods=['GET'])
 def get_pid_output():
